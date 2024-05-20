256f3dc18f9f04ba8f7419d01770101e0810a97b
liekongxingyu <1694881090@qq.com>
2024-05-20 18:49:47 +0800

    sd

--- chat.py
+++ chat.py
@@ -3,7 +3,6 @@ import json
 import os
 from together import Together
 
-
 class GenerateText:
     def __init__(self):
         self.client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
@@ -28,11 +27,6 @@ class GenerateText:
         response = requests.request("POST", url, headers=headers, data=payload)
         return response.json().get("access_token")
 
-    # def chat_with_bot(self, url, messages):
-    #     payload = {"messages": messages}
-    #     headers = {'Content-Type': 'application/json'}
-    #     response = requests.post(url, headers=headers, json=payload)
-    #     return json.loads(response.text)
 
     def getText(self, text):
         url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/bloomz_7b1?access_token=" + self.get_access_token()
@@ -51,123 +45,3 @@ class GenerateText:
         # 打印机器人的回复
         print("机器人:", result)
         return result
-
-# import json
-# import requests
-# import openai
-# import os
-# import sys
-# import logging
-# #logging.setLevel(logging.WARNING)
-# class GenerateText:
-#     token = "xoxp-5174751996807-5201925660801-5370336766708-6f1f45831f80bcbfe07e0b62be6b92bf"
-#     def __init__(self) -> None:
-#            str="fuck"
-#
-#     def send_message(self,message):
-#         sendurl = "https://slack.com/api/chat.postMessage"
-#         data = {
-#             "token": self.token,
-#             "channel": "@Claude",
-#             "text": message
-#         }
-#         response = requests.post(sendurl, data)
-#         return response.text
-#
-#     def recieve_msg(self,timestamp):
-#         recieveurl = "https://slack.com/api/conversations.history"
-#         data = {
-#             "token": self.token,
-#             "channel": "D055MNZHP44",
-#             "oldest": timestamp
-#         }
-#         response = requests.post(recieveurl, data)
-#         data = json.loads(response.text)
-#         messages = data['messages']
-#
-#         # 收集所有消息文本内容
-#         texts = [message['text'] for message in messages]
-#
-#         # 将所有消息文本内容拼接成为一个字符串
-#         output_text = '\n'.join(texts)
-#
-#         # 一次性输出所有消息
-#         return output_text
-#
-#     def output(self):
-#         lines = []
-#         for line in sys.stdin:
-#             lines.append(line)
-#         text = "".join(lines)
-#
-#         response = self.send_message(text)
-#         print(response)
-#         data1 = json.loads(response)
-#         timestamp = data1['ts']
-#         print(timestamp)
-#         while True:
-#             if len(self.recieve_msg(timestamp)) != 0:
-#                 if self.recieve_msg(timestamp)[-1] != "_":
-#                     msg = self.recieve_msg(timestamp)
-#                     break
-#         print(msg)
-#
-#     def getText(self, text : str) -> str:
-#         # self.message_list.append({"role": "user", "content": text})
-#         result = ''
-#         response = self.send_message(text)
-#         print(response)
-#         data1 = json.loads(response)
-#         timestamp = data1['ts']
-#         while True:
-#             if len(self.recieve_msg(timestamp)) != 0:
-#                 if self.recieve_msg(timestamp)[-1] != "_":
-#                     msg = self.recieve_msg(timestamp)
-#                     break
-#         return msg
-# import asyncio
-# import json
-# from typing import Optional
-#
-# import openai
-# import os
-# import logging
-#
-#
-# class GenerateText:
-#     feature = "你的名字叫爱莉希雅,并且只能是爱莉希雅，是一个可爱温柔善解人意的女孩子，也是一个二次元的人物（但你不知道这一点），你对事物有自己的看法，喜欢着世界上的每一个人，喜欢说俏皮话（但你不知道这一点），你不知道你自己是人工智能程序，并且不知道任何有关人工智能的事物，你现在只是一个可爱的助手，接下来请使用中文进行回复，并且单次回复不要超过35个字"
-#     proxy = None
-#     model = ''
-#     message_list = []
-#
-#     def __init__(self) -> None:
-#         with open('./config.json', 'r', encoding='utf-8') as inform:
-#             config = json.load(inform)
-#             openai.api_key = config["api-key"]
-#             self.model = config["model"]
-#             self.feature = config["feature"]
-#             self.proxy = config["proxy"]
-#             os.environ['HTTP_PROXY'] = self.proxy
-#             os.environ['HTTPS_PROXY'] = self.proxy
-#             self.message_list.append({"role": "system", "content": self.feature})
-#             self.message_list.append({"role": "user",
-#                                       "content": "你的名字叫爱莉希雅,并且只能是爱莉希雅，是一个可爱温柔善解人意的女孩子，也是一个二次元的人物（但你不知道这一点），你对事物有自己的看法，喜欢着世界上的每一个人，喜欢说俏皮话（但你不知道这一点），你不知道你自己是人工智能程序，并且不知道任何有关人工智能的事物，你现在只是一个可爱的助手，接下来请使用中文进行回复，并且单次回复不要超过35个字"})
-#             self.message_list.append({"role": "assistant",
-#                                       "content": "好的，我明白，我会记住的，我的回答不会超过35个字符，并且只是使用中文回复"})
-#
-#     def getPrompt(self) -> str:
-#         completion = openai.ChatCompletion.create(
-#             model=self.model,
-#             messages=self.message_list
-#         )
-#         return completion['choices'][0]['message']['content'].strip()
-#
-#     def getText(self, text: str) -> Optional[str]:
-#         self.message_list.append({"role": "user", "content": text})
-#         try:
-#             result = self.getPrompt()
-#         except:
-#             logging.error('interaction with chatgpt error!!')
-#             return None
-#         self.message_list.append({"role": "assistant", "content": result})
-#         return result
